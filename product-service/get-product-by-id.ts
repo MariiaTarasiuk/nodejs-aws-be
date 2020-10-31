43990cbf8d524d481b858f76b0c9c373a22326f5
MariiaTarasiuk <mariia.tarasiuk.1@gmail.com>
2020-10-31 18:23:56 +0200

    add cors headers

--- product-service/get-product-by-id.ts
+++ product-service/get-product-by-id.ts
@@ -1,12 +1,18 @@
 import * as AWSLambda from "aws-lambda";
+import { CORS_HEADERS } from "./cors-headers";
 
 export function getProductByID(event: AWSLambda.APIGatewayEvent, _context: AWSLambda.Context) {
   try {
     return {
       statusCode: 200,
       body: `return product with id - ${event.pathParameters.productId}`,
+      headers: CORS_HEADERS,
     };
   } catch {
-    return { statusCode: 500, error: "ERROR: plz provide product ID" };
+    return {
+      headers: CORS_HEADERS,
+      statusCode: 500,
+      error: "ERROR: provide product ID",
+    };
   }
 }
--- product-service/get-product-list.ts
+++ product-service/get-product-list.ts
@@ -1,8 +1,19 @@
 import * as AWSLambda from "aws-lambda";
+import { CORS_HEADERS } from "./cors-headers";
+import * as productList from "./products.mock.json";
 
 export function getProductList(event: AWSLambda.APIGatewayEvent, _context: AWSLambda.Context) {
-  return {
-    statusCode: 200,
-    body: `mock product list`,
-  };
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(productList),
+      headers: CORS_HEADERS,
+    };
+  } catch {
+    return {
+      headers: CORS_HEADERS,
+      statusCode: 500,
+      error: "ERROR: can't get product list",
+    };
+  }
 }
