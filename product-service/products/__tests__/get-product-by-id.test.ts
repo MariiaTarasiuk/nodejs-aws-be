a4241753a9be7fac971e39536d93edb493127030
MariiaTarasiuk <mariia.tarasiuk.1@gmail.com>
2020-11-05 22:26:24 +0200

    update tests

--- product-service/products/__tests__/get-product-by-id.test.ts
+++ product-service/products/__tests__/get-product-by-id.test.ts
@@ -1,3 +1,4 @@
+import { CORS_HEADERS } from "../../cors-headers";
 import { getProductByID } from "../get-product-by-id";
 jest.mock(
   "../products.mock.json",
@@ -12,15 +13,19 @@ describe("getProductByID", () => {
     expect.assertions(1);
   });
   test("should return status 200 for correct request", async () => {
-    const response = await getProductByID({ pathParameters: { productId: "test" } }, {}, () => ({}));
-    expect(response).toStrictEqual({});
+    const response = await getProductByID({ pathParameters: { productId: "test" } });
+    expect(response).toMatchObject({
+      statusCode: 200,
+      body: `{"id":"test","description":"mock product list"}`,
+      headers: CORS_HEADERS,
+    });
   });
-  test("should return status 404 if no product by id found", async () => {
-    const response = await getProductByID({ pathParameters: { productId: "test 2" } }, {}, () => {});
-    expect(response).toStrictEqual({ error: '"Product with id test 2 not found"', statusCode: 500 });
+  test("should return status 500 if no product by id found", async () => {
+    const response = await getProductByID({ pathParameters: { productId: "test 2" } });
+    expect(response).toMatchObject({ error: '"Product with id test 2 not found"', statusCode: 500 });
   });
   test("should return status 500 if Error", async () => {
-    const response = await getProductByID({ pathParameters: { id: null } }, {}, () => ({}));
-    expect(response).toStrictEqual({ error: '"Product with id undefined not found"', statusCode: 500 });
+    const response = await getProductByID({ pathParameters: { id: null } });
+    expect(response).toMatchObject({ error: '"Product with id undefined not found"', statusCode: 500 });
   });
 });
--- product-service/products/__tests__/get-product-list.test.ts
+++ product-service/products/__tests__/get-product-list.test.ts
@@ -1,21 +1,31 @@
-import { getProductList } from "../get-product-list";
-jest.mock(
-  "../products.mock.json",
-  () => ({
-    products: "mock product list",
-  }),
-  { virtual: true }
-);
+import { CORS_HEADERS } from "../../cors-headers";
 
 describe("getProductList", () => {
+  beforeEach(() => jest.resetModules());
+
   test("should return status 200 for correct request", async () => {
-    expect.assertions(1);
-    const response = await getProductList({}, {}, () => ({}));
-    expect(response).toStrictEqual({});
+    jest.mock("../products.mock.json", () => ({
+      products: "mock product list",
+    }));
+    const { getProductList } = require("../get-product-list.ts");
+    const response = await getProductList({});
+
+    expect(response).toMatchObject({
+      statusCode: 200,
+      body: '"mock product list"',
+      headers: CORS_HEADERS,
+    });
   });
+
   test("should return status 500 if Error", async () => {
-    expect.assertions(1);
-    const response = await getProductList({}, {}, () => ({}));
-    expect(response).toStrictEqual({});
+    jest.mock("../products.mock.json", () => {});
+    const { getProductList } = require("../get-product-list.ts");
+    const response = await getProductList({});
+
+    expect(response).toMatchObject({
+      statusCode: 500,
+      error: "{}",
+      headers: CORS_HEADERS,
+    });
   });
 });
