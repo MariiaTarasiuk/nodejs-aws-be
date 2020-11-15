ed7a4bfdcbf615b8c02ba3f705f9d1838090a027
MariiaTarasiuk <mariia.tarasiuk.1@gmail.com>
2020-11-15 20:22:42 +0200

    add sdk and signedURL handler

--- import-service/import/import-products-file.ts
+++ import-service/import/import-products-file.ts
@@ -1,18 +1,28 @@
 import * as AWSLambda from "aws-lambda";
+import * as AWSSDK from "aws-sdk";
 
 export const importProductsFile = async (event: AWSLambda.APIGatewayEvent) => {
+  const s3 = new AWSSDK.S3({ region: "eu-west-1", signatureVersion: "v4" });
   try {
-    const name = event.queryStringParameters.name;
-    return {
-      statusCode: 200,
-      body: JSON.stringify(name),
-      headers: { "Access-Control-Allow-Origin": "*" },
-    };
+    const { name } = event.queryStringParameters;
+    const params = { Bucket: "app-import-storage", Key: `uploaded/${name}`, Expires: 60, ContentType: "text/csv" };
+
+    return new Promise((res, rej) => {
+      s3.getSignedUrl("putObject", params, (err, url) => {
+        if (err) return rej(err);
+        res({
+          statusCode: 200,
+          headers: { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Credentials": true },
+          body: url,
+        });
+      });
+    });
   } catch (e) {
+    console.log("## ERROR ##", e);
     return {
       statusCode: 500,
-      error: JSON.stringify(Error("bad file name")),
-      headers: { "Access-Control-Allow-Origin": "*" },
+      error: JSON.stringify(Error(e.message)),
+      headers: { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Credentials": true },
     };
   }
 };
