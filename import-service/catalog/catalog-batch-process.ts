122a2ff5ba483a0996283930dfe11e8fec9fcfd8
MariiaTarasiuk <mariia.tarasiuk.1@gmail.com>
2020-11-22 21:24:56 +0200

    add items to DB

--- import-service/catalog/catalog-batch-process.ts
+++ import-service/catalog/catalog-batch-process.ts
@@ -3,28 +3,23 @@ import { Client } from "pg";
 import { dbOptions } from "../helpres/db-config";
 
 export const catalogBatchProcess = async (event: AWSLambda.SQSHandler) => {
-  const products = event.Records.map(({ body }) => JSON.parse(body));
   const DB = new Client(dbOptions);
-  console.log("WHAT IS PRODUCTS :", products, Array.isArray(products));
   try {
     await DB.connect();
-    if (products.length > 0) {
-      console.log("# DB instance # :", dbOptions);
-      await Promise.all(
-        products.map(({ title, description, price, count }) => {
-          const {
-            rows: [{ id }],
-          } = DB.query(`insert into products (title, description, price) values ($1, $2, $3) returning id`, [
-            title,
-            description,
-            price,
-          ]);
-          console.log("# add product to Products DB # ", id);
-          DB.query(`insert into stocks (product_id, count) values ($1, $2)`, [id, count]);
-          console.log(`SUCCESS post product ${title}:`, id);
-        })
-      );
-    }
+    const products = event.Records.map(async ({ body }) => {
+      const { title, description, price, count } = JSON.parse(body);
+      const {
+        rows: [{ id }],
+      } = await DB.query(`insert into products (title, description, price) values ($1, $2, $3) returning id`, [
+        title,
+        description,
+        price,
+      ]);
+      console.log("# add product to Products DB # ", id);
+      await DB.query(`insert into stocks (product_id, count) values ($1, $2)`, [id, count]);
+      console.log(`add product ${title} to stocks:`, id);
+    });
+    const results = await Promise.all(products);
   } catch (error) {
     console.log("SOMETHING GOES WRONG: ", error);
   } finally {
