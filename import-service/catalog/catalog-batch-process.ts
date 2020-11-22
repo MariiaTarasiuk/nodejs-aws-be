de6f4d9d40c74051fdb9c689f3bf3bb9b30dabb4
MariiaTarasiuk <mariia.tarasiuk.1@gmail.com>
2020-11-22 20:03:56 +0200

    change to promise all

--- import-service/catalog/catalog-batch-process.ts
+++ import-service/catalog/catalog-batch-process.ts
@@ -10,18 +10,20 @@ export const catalogBatchProcess = async (event: AWSLambda.SQSHandler) => {
     await DB.connect();
     if (products.length > 0) {
       console.log("# DB instance # :", dbOptions);
-      products.forEach(async ({ title, description, price, count }) => {
-        const {
-          rows: [{ id }],
-        } = await DB.query(`insert into products (title, description, price) values ($1, $2, $3) returning id`, [
-          title,
-          description,
-          price,
-        ]);
-        console.log("# add product to Products DB # ", id);
-        await DB.query(`insert into stocks (product_id, count) values ($1, $2)`, [id, count]);
-        console.log(`SUCCESS post product ${title}:`, id);
-      });
+      await Promise.all(
+        products.map(({ title, description, price, count }) => {
+          const {
+            rows: [{ id }],
+          } = DB.query(`insert into products (title, description, price) values ($1, $2, $3) returning id`, [
+            title,
+            description,
+            price,
+          ]);
+          console.log("# add product to Products DB # ", id);
+          DB.query(`insert into stocks (product_id, count) values ($1, $2)`, [id, count]);
+          console.log(`SUCCESS post product ${title}:`, id);
+        })
+      );
     }
   } catch (error) {
     console.log("SOMETHING GOES WRONG: ", error);
